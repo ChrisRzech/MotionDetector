437de8190b6fb6db0f0c0a90b6d1e973f4e1468a
Christopher Rzechula <christopherrzechula@gmail.com>
2022-04-13 01:07:12 -0700

    Make HCSR04 min and max distance constants public

--- MotionDetector/HCSR04.hpp
+++ MotionDetector/HCSR04.hpp
@@ -4,6 +4,9 @@
 class HCSR04
 {
 public:
+    static constexpr uint16_t minDistance = 2; //Centimeter
+    static constexpr uint16_t maxDistance = 400; //Centimeter
+
     HCSR04(const Port&, Port::Pin trigger, Port::Pin echo);
     
     uint16_t getDistance() const; //Centimeter
@@ -12,8 +15,6 @@ private:
     uint16_t ping() const; //Microsecond
     uint16_t convert(uint16_t time) const; //Centimeter
     
-    static constexpr uint16_t minDistance = 2; //Centimeter
-    static constexpr uint16_t maxDistance = 400; //Centimeter
     static constexpr uint8_t conversionScalar = 58;
 
     const Port& m_port;
