7353678c7efdb7152a1a7da6637ce1a822f822b0
Christopher Rzechula <christopherrzechula@gmail.com>
2022-04-30 15:12:29 -0700

    Fine tune speaker more

--- MotionDetector/Speaker.cpp
+++ MotionDetector/Speaker.cpp
@@ -13,8 +13,8 @@ void Speaker::emit(uint16_t freq, uint16_t duration) const
        of the frequencies isn't good as you increase frequency (see formula for frequency under
        CTC mode on page 74 in ATmega32 documentation) */
     
-    //NOTE hard-code a fine tuning of the frequencies by subtracting 100
-	float period = 1'000'000.f / freq - 100; //microseconds/cycle
+    //NOTE hard-code a fine tuning of the frequencies by subtracting some constant (tuned by ear)
+	uint16_t period = 1'000'000.f / freq - 80; //microseconds/cycle
     uint16_t cycles = duration / (1000.f / freq); //cycles
 	
 	for(uint16_t i = 0; i < cycles; i++)
