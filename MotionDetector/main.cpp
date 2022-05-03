80307da1f00bc2a1fecb24b8bf55586fbb78da51
Christopher Rzechula <christopherrzechula@gmail.com>
2022-05-03 16:04:11 -0700

    Add project driver

--- MotionDetector/main.cpp
+++ MotionDetector/main.cpp
@@ -1,9 +1,71 @@
+#include "S01602DTR.hpp"
+#include "HCSR04.hpp"
 #include "Speaker.hpp"
+#include <stdio.h>
+
+constexpr uint16_t g_sonarDelay = 70;
+
+void printDistance(const S01602DTR& lcd, uint16_t distance)
+{
+    char buf[17] = {'\0'};
+    int8_t len = snprintf(buf, 17, "%ucm", distance);
+    for(int8_t i = len; i < 16; i++)
+        buf[i] = ' ';
+    lcd << buf;
+}
+
+uint16_t getDistance(const HCSR04& sonar)
+{
+    uint16_t distance;
+    do
+    {
+        distance = sonar.getDistance();
+    } while(distance == UINT16_MAX);
+    return distance;
+}
+
+uint16_t calibrate(const HCSR04& sonar, uint16_t duration)
+{
+    uint16_t sampleCount = duration / g_sonarDelay;
+    uint16_t sum = 0;
+    
+    for(uint16_t i = 1; i <= sampleCount; i++)
+    {
+        uint16_t distance = getDistance(sonar);
+        sum += distance;
+        
+        waitMs(g_sonarDelay);
+    }
+    
+    return sum / sampleCount;
+}
 
 int main()
 {
+    S01602DTR lcd(portD, portB, Port::Pin::_0, Port::Pin::_1, Port::Pin::_2);
+    HCSR04 sonar(portC, Port::Pin::_0, Port::Pin::_1);
     Speaker speaker(portB, Port::Pin::_3);
+    
+    lcd << "Calibrating...";
+    uint16_t calibratedDistance = calibrate(sonar, 1000);
+    constexpr uint16_t deviation = 10;
 
-    for(uint16_t freq = 55; freq < 1760; freq += 2)
-        speaker.emit(freq, 10);
+    lcd.clear();
+    lcd << "Cali: ";
+    printDistance(lcd, calibratedDistance);
+    
+    while(true)
+    {
+        uint16_t distance = getDistance(sonar);
+        
+        lcd.setCursorPosition(1, 0);
+        lcd << "Dist: ";
+        printDistance(lcd, distance);
+        
+        //Motion detected when distance is outside of deviation range
+        if(distance < calibratedDistance - deviation || distance > calibratedDistance + deviation)
+            speaker.emit(1000, 50);
+        
+        waitMs(g_sonarDelay);
+    }
 }
